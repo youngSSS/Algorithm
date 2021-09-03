136a90947384b2d599e353df99c701d77a7ca8fa
YoungSSS <young4127s@naver.com>
2021-09-03 15:14:53 +0900

    kakao 2020 blind

--- Programmers/자물쇠와 열쇠.cpp
+++ Programmers/자물쇠와 열쇠.cpp
@@ -1,168 +1,71 @@
 #include <vector>
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
+vector< vector< vector<int> > > rotate_key(vector<vector<int>> key) {
+    vector< vector< vector<int> > > result;
+    int key_size = key.size();
+    result.push_back(key);
+
+    for (int i = 0; i < 3; i++) {
+        for (int x = 0; x < key_size; x++) {
+            for (int y = 0; y < key_size; y++) {
+                int nx = -y + key_size - 1;
+                int ny = x;
+                key[nx][ny] = result[i][x][y];
+            }
+        }
+        result.push_back(key);
+    }
+
+    return result;
+}
+
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
-    vector<vector<int>> rotated_key(key.size());
-    vector<int> bit_key[4];
-    vector<int> bit_lock;
+    int k_size = key.size();
+    int l_size = lock.size();
+    int ans_cnt = 0;
+    vector< vector< vector<int> > > keys = rotate_key(key);
+    vector< vector<int> > extended_lock(2 * k_size + l_size - 2, vector<int>(2 * k_size + l_size - 2, 2));
 
-    for (int i = 0; i < key.size(); i++) {
-        for (int j = key.size() - 1; j >= 0; j--)
-            rotated_key[i].push_back(key[j][i]);
+    for (int x = k_size - 1; x < k_size - 1 + l_size; x++) {
+        for (int y = k_size - 1; y < k_size - 1 + l_size; y++)
+            extended_lock[x][y] = lock[x - k_size + 1][y - k_size + 1];
     }
 
-    for (int i = 0; i < key.size(); i++) {
-        int value1 = 0, value2 = 0;
-        for (int j = 0; j < key[i].size(); j++) {
-            if (key[i][j] == 1) value1 += pow(2, key[i].size() - j - 1);
-            if (rotated_key[i][j] == 1) value2 += pow(2, key[i].size() - j - 1);
-        }
-        bit_key[0].push_back(value1 * pow(2, lock.size() - key.size()));
-        bit_key[2].push_back(value2 * pow(2, lock.size() - key.size()));
-    }
-    for (int i = key.size() - 1; i >= 0; i--) {
-        int value1 = 0, value2 = 0;
-        for (int j = key[i].size() - 1; j >= 0; j--) {
-            if (key[i][j] == 1) value1 += pow(2, j);
-            if (rotated_key[i][j] == 1) value2 += pow(2, j);
-        }
-        bit_key[1].push_back(value1 * pow(2, lock.size() - key.size()));
-        bit_key[3].push_back(value2 * pow(2, lock.size() - key.size()));
+    for (const auto& lock_row : lock) {
+        for (auto l : lock_row)
+            if (l == 0) ans_cnt += 1;
     }
 
-    for (int i = 0; i < 2 * lock.size() - key.size(); i++) {
-        for (int j = 0; j < 4; j++) bit_key[j].push_back(0);
-    }
+    for (auto k : keys) {
+        int lx_start = 0;
+        int ly_start = 0;
 
-    for (int j = 0; j < lock.size(); j++) {
-        int value = 0;
-        for (int i = 0; i < lock[j].size(); i++) {
-            if (lock[j][i] == 1) value += pow(2, lock.size() - i - 1);
-        }
-        bit_lock.push_back(value);
-    }
+        while (lx_start + (k_size - 1) < extended_lock.size()) {
+            while (ly_start + (k_size - 1) < extended_lock.size()) {
 
-    for (auto & i : bit_key) {
-        for (int k : i) {
-            bitset<3> t(k);
-            cout << t << endl;
-        }
-        cout << endl;
-    }
+                int cnt = 0;
+                for (int x = 0; x < k_size; x++) {
+                    for (int y = 0; y < k_size; y++) {
+                        if (k[x][y] == 1 && extended_lock[lx_start + x][ly_start + y] == 0) cnt += 1;
+                        else if (k[x][y] == 1 && extended_lock[lx_start + x][ly_start + y] == 1) {
+                            cnt = -1;
+                            break;
+                        }
+                    }
 
-    vector<int> temp[4];
-    temp[0] = bit_key[0];
-    temp[1] = bit_key[1];
-    temp[2] = bit_key[2];
-    temp[3] = bit_key[3];
-    for (int z = 0; z < key.size() - 1; z++) {
-        for (int m = 0; m < 4; m++) {
-            temp[m].erase(temp[m].begin());
-            temp[m].push_back(0);
-        }
-
-        cout << "SDF " << z << endl;
-        for (auto & i : temp) {
-            for (int k : i) {
-                bitset<3> t(k);
-                cout << t << endl;
-            }
-            cout << endl;
-        }
-
-        for (int i = 0; i < key.size(); i++) {
-            int cnt = 15, rotated_cnt = 15;
-            for (int l = 0; l < bit_lock.size(); l++) {
-                for (int j = 0; j < 4; j++) {
-                    if (((temp[j][l] << i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        cnt &= ~(1 << j);
-                    if (((temp[j][l] << i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        rotated_cnt &= ~(1 << j);
-                    cout << cnt << " " << rotated_cnt << endl;
-                    cout << bitset<4>(cnt) << " " << bitset<4>(rotated_cnt) << endl;
+                    if (cnt == -1) break;
                 }
-                if (cnt == 0 && rotated_cnt == 0) break;
-            }
-            if (cnt != 0 || rotated_cnt != 0) {
-                cout << "!!!!!" << cnt << " " << rotated_cnt << endl;
-                cout << bitset<4>(cnt) << " " << bitset<4>(rotated_cnt) << endl;
-                return true;
-            }
-        }
-        for (int i = 0; i < key.size(); i++) {
-            int cnt = 15, rotated_cnt = 15;
-            for (int l = 0; l < bit_lock.size(); l++) {
-                for (int j = 0; j < 4; j++) {
-                    if (((temp[j][l] >> i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        cnt &= ~(1 << j);
-                    if (((temp[j][l] >> i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        rotated_cnt &= ~(1 << j);
-                }
-                if (cnt == 0 && rotated_cnt == 0) break;
-            }
-            if (cnt != 0 || rotated_cnt != 0) return true;
-        }
-    }
 
-    for (int i = 0; i < key.size(); i++) {
-        int cnt = 15, rotated_cnt = 15;
-        for (int l = 0; l < bit_lock.size(); l++) {
-            for (int j = 0; j < 4; j++) {
-                if (((bit_key[j][l] << i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                    cnt &= ~(1 << j);
-                if (((bit_key[j][l] << i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                    rotated_cnt &= ~(1 << j);
-            }
-            if (cnt == 0 && rotated_cnt == 0) break;
-        }
-        if (cnt != 0 || rotated_cnt != 0) return true;
-    }
-    for (int i = 0; i < key.size(); i++) {
-        int cnt = 15, rotated_cnt = 15;
-        for (int l = 0; l < bit_lock.size(); l++) {
-            for (int j = 0; j < 4; j++) {
-                if (((bit_key[j][l] >> i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                    cnt &= ~(1 << j);
-                if (((bit_key[j][l] >> i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                    rotated_cnt &= ~(1 << j);
-            }
-            if (cnt == 0 && rotated_cnt == 0) break;
-        }
-        if (cnt != 0 || rotated_cnt != 0) return true;
-    }
+                if (cnt == ans_cnt) return true;
 
-    for (int z = 0; z < lock.size(); z++) {
-        for (int m = 0; m < 4; m++)
-            bit_key[m].insert(bit_key[m].begin(), 0);
+                ly_start += 1;
+            }
 
-        for (int i = 0; i < key.size(); i++) {
-            int cnt = 15, rotated_cnt = 15;
-            for (int l = 0; l < bit_lock.size(); l++) {
-                for (int j = 0; j < 4; j++) {
-                    if (((bit_key[j][l] << i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        cnt &= ~(1 << j);
-                    if (((bit_key[j][l] << i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        rotated_cnt &= ~(1 << j);
-                }
-                if (cnt == 0 && rotated_cnt == 0) break;
-            }
-            if (cnt != 0 || rotated_cnt != 0) return true;
-        }
-        for (int i = 0; i < key.size(); i++) {
-            int cnt = 15, rotated_cnt = 15;
-            for (int l = 0; l < bit_lock.size(); l++) {
-                for (int j = 0; j < 4; j++) {
-                    if (((bit_key[j][l] >> i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        cnt &= ~(1 << j);
-                    if (((bit_key[j][l] >> i) ^ bit_lock[l]) != pow(2, bit_lock.size()) - 1)
-                        rotated_cnt &= ~(1 << j);
-                }
-                if (cnt == 0 && rotated_cnt == 0) break;
-            }
-            if (cnt != 0 || rotated_cnt != 0) return true;
+            lx_start += 1;
+            ly_start = 0;
         }
     }
 
@@ -170,6 +73,6 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
 }
 
 int main() {
-    cout << solution({{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}, {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
+    cout << solution({{0, 0, 0, 1}, {1, 0, 0, 1}, {0, 1, 1, 0}, {0, 1, 1, 0}}, {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
     return 0;
 }
