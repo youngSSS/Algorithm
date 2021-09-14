a44bfcb9cfa8e0dc25bc888abfe08c901a222fda
YoungSSS <young4127s@naver.com>
2021-09-14 22:25:12 +0900

    kakao 2022 blind 복기

--- Kakao 2021/kakao_3.cpp
+++ Kakao 2021/kakao_3.cpp
@@ -60,3 +60,16 @@ vector<int> solution(vector<int> fees, vector<string> records) {
 
 	return answer;
 }
+
+int main() {
+	vector<int> answer = solution(
+		{ 180, 5000, 10, 600 },
+		{
+			"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT",
+			"07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN",
+			"19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"
+		}
+	);
+	for (auto ans : answer) cout << ans << " ";
+	return 0;
+}
