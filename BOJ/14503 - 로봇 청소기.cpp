3367c6e9542324615ca9e3ba83bd00d2d7ca9fd9
YoungSSS <young4127s@naver.com>
2021-10-16 01:58:01 +0900

    Samsung SW

--- BOJ/14503 - 로봇 청소기.cpp
+++ BOJ/14503 - 로봇 청소기.cpp
@@ -1,37 +1,37 @@
 #include <vector>
 #include <unordered_map>
 #include <cstdio>
-#include <iostream>
 
 using namespace std;
 
 int n, m, ans = 0;
 unordered_map<int, pair<pair<int, int>, pair<int, int>>> dir;
-vector<vector<int>> map, visit;
+vector<vector<int>> map;
 
 void do_clean(int x, int y, int d) {
-	visit[x][y] = 1;
+	int cnt, nx, ny;
+
+	map[x][y] = 2;
 	ans += 1;
 
-	int cnt;
 	for (cnt = 0; cnt < 4; cnt++) {
 		d -= 1;
 		if (d == -1) d = 3;
 
-		int nx = x + dir[d].first.first;
-		int ny = y + dir[d].first.second;
+		nx = x + dir[d].first.first;
+		ny = y + dir[d].first.second;
 
-		if (map[nx][ny] == 0 && visit[nx][ny] == 0) {
+		if (map[nx][ny] == 0) {
 			do_clean(nx, ny, d);
 			break;
 		}
 	}
 
 	if (cnt == 4) {
-		int nx = x + dir[d].second.first;
-		int ny = y + dir[d].second.second;
+		nx = x + dir[d].second.first;
+		ny = y + dir[d].second.second;
 
-		if (map[nx][ny] == 0) {
+		if (map[nx][ny] != 1) {
 			ans -= 1;
 			do_clean(nx, ny, d);
 		}
@@ -44,8 +44,6 @@ int main() {
 	scanf("%d %d %d", &x, &y, &d);
 
 	map.resize(n, vector<int>(m));
-	visit.resize(n, vector<int>(m));
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++)
 			scanf("%d", &map[i][j]);
